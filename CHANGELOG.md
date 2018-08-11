dc0e8d1da9eecbd9274514b8737d98049bd8bbc1
Eric Stout <ericwstout@gmail.com>
2018-08-11 16:46:12 -0700

    Release/1.0.0 (#10)
    * add flow support, update handler methods to use state, update to use react-jss
    
    * Update test command

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,26 +1,50 @@
 # Changelog
+
 All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
-## [0.3.0] - 2018-03-12
-### Fix
-- Adjust webpack config to ensure when compiled for production, only React production
-code is included
+## [1.0.0] - 2018-08-11
 
 ### Added
-- className support thanks to @tamirkash
+
+* Flow support
+* React-JSS for styling
+
+### Updated
+
+* All handler methods to use state instead of class switching
+
+### Removed
+
+* CSS Imports
+
+## [0.3.0] - 2018-03-12
+
+### Fix
+
+* Adjust webpack config to ensure when compiled for production, only React production
+  code is included
+
+### Added
+
+* className support thanks to @tamirkash
 
 ## [0.2.0] - 2018-03-12
+
 ### Fix
-- Webpack config that was causing error(s) when trying to import as a npm package.
+
+* Webpack config that was causing error(s) when trying to import as a npm package.
 
 ## [0.1.1] - 2018-03-12
-### Added
-- This CHANGELOG
-- production build
 
+### Added
+
+* This CHANGELOG
+* production build
+
+[0.3.0]: https://github.com/factor1/react-card-flipper/compare/v0.3.0...v1.0.0
 [0.3.0]: https://github.com/factor1/react-card-flipper/compare/v0.2.0...v0.3.0
 [0.2.0]: https://github.com/factor1/react-card-flipper/compare/v0.1.1...v0.2.0
 [0.1.1]: https://github.com/factor1/react-card-flipper/compare/v0.1.0...v0.1.1
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,19 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = env => {
   return {
-    entry: env.production ? ['babel-polyfill', './ReactCardFlipper.js'] : ['babel-polyfill', './Test.js'],
+    entry: env.production ? ["./ReactCardFlipper.js"] : ["./Test.js"],
     output: {
-      filename: env.production ? 'ReactCardFlipper.js' : 'test_bundle.js',
-      path: env.production ? path.resolve(__dirname, 'dist') : path.resolve(__dirname, 'tests'),
-      library: 'ReactCardFlipper',
-      libraryTarget: 'umd'
+      filename: env.production ? "ReactCardFlipper.js" : "test_bundle.js",
+      path: env.production
+        ? path.resolve(__dirname, "dist")
+        : path.resolve(__dirname, "tests"),
+      library: "ReactCardFlipper",
+      libraryTarget: "umd"
     },
     devServer: {
-      contentBase: './tests'
+      contentBase: "./tests"
     },
     module: {
       rules: [
@@ -19,16 +21,22 @@ module.exports = env => {
         {
           test: /\.css$/,
           use: [
-            'style-loader',
-            { loader: 'css-loader', options: { importLoaders: 1 } },
-            'postcss-loader'
+            "style-loader",
+            { loader: "css-loader", options: { importLoaders: 1 } },
+            "postcss-loader"
           ]
         }
       ]
     },
     plugins: [
-      env.production ? new webpack.DefinePlugin({"process.env": {NODE_ENV: JSON.stringify("production")}}) : '',
-      env.production ? new webpack.optimize.UglifyJsPlugin() : '',
+      env.production
+        ? new webpack.DefinePlugin({
+            "process.env": { NODE_ENV: JSON.stringify("production") }
+          })
+        : new webpack.DefinePlugin({
+            "process.env": { NODE_ENV: JSON.stringify("develop") }
+          }),
+      new webpack.optimize.UglifyJsPlugin()
     ]
-  }
+  };
 };
